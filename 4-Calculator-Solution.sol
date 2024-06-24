1684fdb681e62ef3c166143d824c07934cbd6848
Vasisht <131481171+vasibhat@users.noreply.github.com>
2024-06-24 22:05:47 +0530

    Calculator.sol

--- 4-Calculator-Solution.sol
+++ 4-Calculator-Solution.sol
@@ -2,10 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// 0. Make a contract called Calculator ✅
-// 1. Create Result variable to store result ✅
-// 2. Create functions to add, subtract, and multiply to result ✅
-// 3. Create a function to get result ✅
 
 contract Calculator {
 
